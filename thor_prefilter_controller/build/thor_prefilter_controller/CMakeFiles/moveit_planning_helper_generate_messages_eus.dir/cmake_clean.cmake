0c9d1b2b2bbada0d3e34165e1f0480d8d2fae969
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2020-07-27 21:40:32 +0200

    nicola: travis

--- thor_prefilter_controller/build/thor_prefilter_controller/CMakeFiles/moveit_planning_helper_generate_messages_eus.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/moveit_planning_helper_generate_messages_eus.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- thor_prefilter_controller/build/thor_prefilter_controller/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wsl/ctrl_ws/src/thor_prefilter_controller
-# Build directory: /home/wsl/ctrl_ws/src/thor_prefilter_controller/build/thor_prefilter_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
